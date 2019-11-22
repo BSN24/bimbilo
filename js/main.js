b39b11957dce03a3590783f65e80c33a6510c9d8
bsn24 <prizrak24@gmail.com>
2019-11-22 11:02:26 +0600

    Добавил страницу карточки товара

--- js/main.js
+++ js/main.js
@@ -5,10 +5,7 @@ $(document).ready(function () {
         disableOnMobile: false,
         nativeOnMobile: false,
         responsive: true,
-
     });
-
-
 
     //Mobile menu
     (function () {
@@ -33,4 +30,41 @@ $(document).ready(function () {
 
         });
     })();
+
+    $(".product__slider-main").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: ".product__slider-nav",
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    dots: false,
+                    fade: false,
+                    arrows: true,
+                    prevArrow: "<button class='product__slider-btn product__slider-btn--prev fa fa-chevron-left'>",
+                    nextArrow: "<button class='product__slider-btn product__slider-btn--next fa fa-chevron-right'>",
+                }
+            }
+        ]
+    });
+
+    $(".product__slider-nav").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: ".product__slider-main",
+        dots: false,
+        arrows: false,
+        centerMode: false,
+        focusOnSelect: true,
+        infinite: false,
+        draggable: false
+    });
+
+    $(["data-fancybox"]).fancybox({
+
+    });
 });
